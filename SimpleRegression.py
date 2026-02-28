1831086fe7f5f8a9e070140a5b19e0a9a5c5f98a
ansarais <sheikh.ansar016@gmail.com>
2026-02-28 13:53:59 +0530

    Code till prediction and visualization is done

--- SimpleRegression.py
+++ SimpleRegression.py
@@ -5,4 +5,24 @@ import matplotlib.pyplot as plt
 # Importing the dataset
 dataset = pd.read_csv('house.csv')
 
-print(dataset.head())
+
+
+# Selection of Regression model
+from sklearn.linear_model import LinearRegression
+
+regression_model = LinearRegression()
+
+# Fitting the model
+
+regression_model.fit(dataset[['Area']], dataset['Price'])
+
+# Predicting the result
+predicted_price = regression_model.predict([[2500]])
+print(predicted_price)
+# Visualising the Training set results
+plt.scatter(dataset['Area'], dataset['Price'], color='red') 
+plt.plot(dataset['Area'], regression_model.predict(dataset[['Area']]), color='blue')
+plt.title('Area vs Price (Training set)')   
+plt.xlabel('Area')
+plt.ylabel('Price')
+plt.show()
